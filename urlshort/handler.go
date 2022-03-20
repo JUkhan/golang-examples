7691dc59f5e22fc7ed2ab77b0cb4a755119817f6
JUkhan <jasim.uddin.khan@gmail.com>
2022-03-20 07:38:10 +0600

    intro

---
+++ urlshort/handler.go
@@ -0,0 +1,5 @@
+package urlshort
+
+func YAMLHandler()  {
+	
+}
---
+++ urlshort/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"https://github.com/JUkhan/golang-examples/urlshort"
+)
+
+func main() {
+	fmt.Println("hi there")
+	
+	YAMLHandler()
+}
